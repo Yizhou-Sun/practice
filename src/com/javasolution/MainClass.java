c23911b0c14ef7f3ab26b5c1dfb470aaccd64d82
Sun, Yizhou <yizhous@telenav.com>
2018-09-07 13:24:03 -0700

    To 23

--- src/com/javasolution/MainClass.java
+++ src/com/javasolution/MainClass.java
@@ -7,17 +7,18 @@ import com.javasolution.util.*;
 
 public class MainClass {
     public static void main(String[] args) {
-        int num = 231;
-        // int[] nums = {10,9,2,5,3,7,101,18};
+        // int num = 231;
+        // int[] nums = {1,8,6,2,5,4,8,3,7};
         // int[][] twodArr = {{2,9,10}, {3,7,15}, {5,12,12}, {15,20,10}, {19,24,8}};
         // String s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
-        // List<String> wordList = Arrays.asList("hot","dot","dog","lot","log","cog");
+        // List<String> strList = Arrays.asList("hot","dot","dog","lot","log","cog");
         // char[][] grid = {{'0','0','0','1'},{'1','1','0','1'},{'1','1','1','1'},{'0','1','1','1'},{'0','1','1','1'}};
         // TreeNode root = stringToTreeNode("[5,3,6,2,4,null,null,1]");
-        var nums1 = new int[] {1,3};
-        var nums2 = new int[] {2};
+        // var nums1 = new int[] {1,3};
+        // var nums2 = new int[] {2};
+
         Solution solution = new Solution();
-        var res = solution.myAtoi("2147483648");
+        var res = solution.generateParenthesis(4);
         System.out.println(res);
     }
     public static TreeNode stringToTreeNode(String input) {
--- src/com/javasolution/Solution.java
+++ src/com/javasolution/Solution.java
@@ -13,7 +13,7 @@ public class Solution {
 
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(nums[i])) {
-                return (new int[]{map.get(nums[i]), i});
+                return (new int[] { map.get(nums[i]), i });
             } else {
                 map.put(target - nums[i], i);
             }
@@ -43,7 +43,8 @@ public class Solution {
         return head.next;
     }
 
-    // 3 https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+    // 3
+    // https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
     public int lengthOfLongestSubstring(String s) {
         HashMap<Character, Integer> map = new HashMap<>();
         int head = 0;
@@ -63,20 +64,25 @@ public class Solution {
     }
 
     // 4 https://leetcode.com/problems/median-of-two-sorted-arrays/description/
-    // TODO: Review
+    // TODO: Review this problem
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int m = nums1.length, n = nums2.length;
         return (getKthNum(nums1, 0, nums2, 0, (m + n + 1) / 2) + getKthNum(nums1, 0, nums2, 0, (m + n + 2) / 2)) / 2.0;
     }
 
     public double getKthNum(int[] nums1, int s1, int[] nums2, int s2, int k) {
-        if (s1 >= nums1.length) return nums2[s2 + k - 1];
-        if (s2 >= nums2.length) return nums1[s1 + k - 1];
-        if (k == 1) return Math.min(nums1[s1], nums2[s2]);
+        if (s1 >= nums1.length)
+            return nums2[s2 + k - 1];
+        if (s2 >= nums2.length)
+            return nums1[s1 + k - 1];
+        if (k == 1)
+            return Math.min(nums1[s1], nums2[s2]);
 
         int mid1 = Integer.MAX_VALUE, mid2 = Integer.MAX_VALUE;
-        if (s1 + k / 2 - 1 < nums1.length) mid1 = nums1[s1 + k / 2 - 1];
-        if (s2 + k / 2 - 1 < nums2.length) mid2 = nums2[s2 + k / 2 - 1];
+        if (s1 + k / 2 - 1 < nums1.length)
+            mid1 = nums1[s1 + k / 2 - 1];
+        if (s2 + k / 2 - 1 < nums2.length)
+            mid2 = nums2[s2 + k / 2 - 1];
         if (mid1 < mid2) {
             return getKthNum(nums1, s1 + k / 2, nums2, s2, k - k / 2);
         } else {
@@ -87,7 +93,8 @@ public class Solution {
     // 5 https://leetcode.com/problems/longest-palindromic-substring/description/
     public String longestPalindromeOn2(String s) {
         int len = s.length();
-        if (len == 0) return "";
+        if (len == 0)
+            return "";
         boolean[][] dp = new boolean[len][len];
         int maxLen = 1;
         int head = 1, tail = 0;
@@ -124,7 +131,6 @@ public class Solution {
                 rIndex = i + P[i];
             }
         }
-
         int maxLen = 0;
         for (int i = 1; i < str.length() - 1; i++) {
             if (P[i] > maxLen) {
@@ -149,7 +155,8 @@ public class Solution {
 
     // 6 https://leetcode.com/problems/zigzag-conversion/description/
     public String convert(String s, int numRows) {
-        if (numRows == 1) return s;
+        if (numRows == 1)
+            return s;
         int delta = 2 * numRows - 2;
         StringBuilder sb = new StringBuilder();
 
@@ -186,9 +193,10 @@ public class Solution {
 
     // 8 https://leetcode.com/problems/string-to-integer-atoi/description/
     public int myAtoi(String str) {
-        int index = 0, sign = 1, total = 0, prev = 0;
+        int index = 0, sign = 1, total = 0;
 
-        if (str.length() == 0) return 0; // empty
+        if (str.length() == 0)
+            return 0; // empty
         while (index < str.length() && str.charAt(index) == ' ') // remove white spaces
             index++;
         if (index < str.length() && (str.charAt(index) == '+' || str.charAt(index) == '-')) { // handle sign
@@ -198,7 +206,8 @@ public class Solution {
 
         while (index < str.length()) {
             int digit = str.charAt(index) - '0';
-            if (digit < 0 || digit > 9) break;
+            if (digit < 0 || digit > 9)
+                break;
             if (Integer.MAX_VALUE / 10 < total || Integer.MAX_VALUE / 10 == total && Integer.MAX_VALUE % 10 < digit)
                 return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             total = total * 10 + digit;
@@ -209,36 +218,402 @@ public class Solution {
 
     // 9 https://leetcode.com/problems/palindrome-number/description/
     public boolean isPalindrome(int x) {
-        return false;
+        if (x < 0)
+            return false;
+        int resx = 0;
+        int orix = x;
+        while (x != 0) {
+            resx = resx * 10 + x % 10;
+            x = x / 10;
+        }
+        return orix == resx;
     }
 
     // 10 https://leetcode.com/problems/regular-expression-matching/description/
+    // TODO: String matching! Dynamic programing
     public boolean isMatch(String s, String p) {
-        return false;
+        int m = s.length();
+        int n = p.length();
+
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= p.length(); i++) {
+            if (p.charAt(i - 1) == '*' && dp[0][i - 2]) {
+                dp[0][i] = true;
+            }
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                char schar = s.charAt(i - 1);
+                char pchar = p.charAt(j - 1);
+
+                if (pchar == '*' && j > 1) {
+                    dp[i][j] = dp[i][j - 2] || dp[i][j - 1];
+                    if (p.charAt(j - 2) == s.charAt(i - 1) || p.charAt(j - 2) == '.') {
+                        dp[i][j] |= dp[i - 1][j];
+                    }
+                } else {
+                    dp[i][j] = (pchar == '.' || schar == pchar) && dp[i - 1][j - 1];
+                }
+            }
+        }
+        return dp[m][n];
     }
 
     // 11 https://leetcode.com/problems/container-with-most-water/description/
     public int maxArea(int[] height) {
-        return 0;
+        int res = 0;
+        int i = 0, j = height.length - 1;
+        while (i < j) {
+            if (height[i] < height[j]) {
+                res = Math.max(res, height[i] * (j - i));
+                i++;
+            } else {
+                res = Math.max(res, height[j] * (j - i));
+                j--;
+            }
+        }
+        return res;
     }
 
     // 12 https://leetcode.com/problems/integer-to-roman/description/
     public String intToRoman(int num) {
-        return "";
+        String M[] = { "", "M", "MM", "MMM" };
+        String C[] = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+        String X[] = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+        String I[] = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+        return M[num / 1000] + C[(num % 1000) / 100] + X[(num % 100) / 10] + I[num % 10];
     }
+
     // 13 https://leetcode.com/problems/roman-to-integer/description/
     public int romanToInt(String s) {
-        return 0;
+        int res = 0;
+        if (s.indexOf("IV") != -1)
+            res -= 2;
+        if (s.indexOf("IX") != -1)
+            res -= 2;
+        if (s.indexOf("XL") != -1)
+            res -= 20;
+        if (s.indexOf("XC") != -1)
+            res -= 20;
+        if (s.indexOf("CD") != -1)
+            res -= 200;
+        if (s.indexOf("CM") != -1)
+            res -= 200;
+
+        for (int count = 0; count < s.length(); count++) {
+            char c = s.charAt(count);
+            if (c == 'M')
+                res += 1000;
+            if (c == 'D')
+                res += 500;
+            if (c == 'C')
+                res += 100;
+            if (c == 'L')
+                res += 50;
+            if (c == 'X')
+                res += 10;
+            if (c == 'V')
+                res += 5;
+            if (c == 'I')
+                res += 1;
+        }
+        return res;
     }
 
     // 14 https://leetcode.com/problems/longest-common-prefix/description/
     public String longestCommonPrefix(String[] strs) {
-        return false;
+        if (strs.length == 0)
+            return "";
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (String str : strs) {
+                if (i >= str.length() || c != str.charAt(i)) {
+                    return sb.toString();
+                }
+            }
+            sb.append(c);
+        }
+        return sb.toString();
     }
 
     // 15 https://leetcode.com/problems/3sum/description/
+    // TODO: how to eliminate duplication!
     public List<List<Integer>> threeSum(int[] nums) {
-        return false;
+        List<List<Integer>> res = new LinkedList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i + 2 < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) { // skip same result
+                continue;
+            }
+            int j = i + 1, k = nums.length - 1;
+            int target = -nums[i];
+            while (j < k) {
+                if (nums[j] + nums[k] == target) {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++; // skip same result
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--; // skip same result
+                } else if (nums[j] + nums[k] > target) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return res;
     }
 
+    // 16 https://leetcode.com/problems/3sum-closest/description/
+    public int threeSumClosest(int[] nums, int target) {
+        int result = nums[0] + nums[1] + nums[nums.length - 1];
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int start = i + 1, end = nums.length - 1;
+            while (start < end) {
+                int sum = nums[i] + nums[start] + nums[end];
+                if (sum > target) {
+                    end--;
+                } else {
+                    start++;
+                }
+                if (Math.abs(sum - target) < Math.abs(result - target)) {
+                    result = sum;
+                }
+            }
+        }
+        return result;
+    }
+
+    // 17
+    // https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+    public List<String> letterCombinations(String digits) {
+        List<String> res = new LinkedList<>();
+        Map<Character, List<Character>> map = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
+
+        map.put('2', Arrays.asList('a', 'b', 'c'));
+        map.put('3', Arrays.asList('d', 'e', 'f'));
+        map.put('4', Arrays.asList('g', 'h', 'i'));
+        map.put('5', Arrays.asList('j', 'k', 'l'));
+        map.put('6', Arrays.asList('m', 'n', 'o'));
+        map.put('7', Arrays.asList('p', 'q', 'r', 's'));
+        map.put('8', Arrays.asList('t', 'u', 'v'));
+        map.put('9', Arrays.asList('w', 'x', 'y', 'z'));
+        letterCombinations(digits, 0, map, sb, res);
+        return res;
+    }
+
+    private void letterCombinations(String digits, int index, Map<Character, List<Character>> map, StringBuilder sb,
+            List<String> res) {
+        if (index == digits.length()) {
+            res.add(sb.toString());
+            return;
+        }
+
+        for (char c : map.get(digits.charAt(index))) {
+            sb.append(c);
+            letterCombinations(digits, index + 1, map, sb, res);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+
+    // 18 https://leetcode.com/problems/4sum/description/
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new LinkedList<>();
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            fourSumHelper(nums, target - nums[i], i + 1, res, nums[i]);
+        }
+        return res;
+    }
+
+    private void fourSumHelper(int[] nums, int t, int index, List<List<Integer>> res, int cur) {
+        for (int i = index; i < nums.length - 2; i++) {
+            if (i > index && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int target = t - nums[i];
+            int start = i + 1;
+            int end = nums.length - 1;
+
+            while (start < end) {
+                int sum = nums[start] + nums[end];
+                if (sum == target) {
+                    res.add(Arrays.asList(cur, nums[i], nums[start], nums[end]));
+                    start++;
+                    end--;
+                    while (start < end && nums[start] == nums[start - 1]) {
+                        start++;
+                    }
+                    while (start < end && nums[end] == nums[end + 1]) {
+                        end--;
+                    }
+                } else if (sum < target) {
+                    start++;
+                } else {
+                    end--;
+                }
+            }
+        }
+    }
+
+    // 19
+    // https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode h1 = head, h2 = head;
+        while (n-- > 0)
+            h2 = h2.next;
+        if (h2 == null)
+            return head.next; // The head need to be removed, do it.
+        h2 = h2.next;
+
+        while (h2 != null) {
+            h1 = h1.next;
+            h2 = h2.next;
+        }
+        h1.next = h1.next.next; // the one after the h1 need to be removed
+        return head;
+    }
+
+    // 20 https://leetcode.com/problems/valid-parentheses/description/
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (st.isEmpty()) {
+                st.push(c);
+            } else if (c == ')' && st.peek() == '(') {
+                st.pop();
+            } else if (c == '}' && st.peek() == '{') {
+                st.pop();
+            } else if (c == ']' && st.peek() == '[') {
+                st.pop();
+            } else {
+                st.push(c);
+            }
+        }
+        return st.isEmpty();
+    }
+
+    // 21 https://leetcode.com/problems/merge-two-sorted-lists/description/
+    // Node: Recursion solution is more elegant
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+        // Iteration
+        // ListNode dummy = new ListNode(0);
+        // ListNode cur = dummy;
+        // while (l1 != null && l2 != null) {
+        // if (l1.val < l2.val) {
+        // cur.next = l1;
+        // cur = l1;
+        // l1 = l1.next;
+        // } else {
+        // cur.next = l2;
+        // cur = l2;
+        // l2 = l2.next;
+        // }
+        // }
+        // if (l1 != null) {
+        // cur.next = l1;
+        // } else {
+        // cur.next = l2;
+        // }
+        // return dummy.next;
+    }
+
+    // 22 https://leetcode.com/problems/generate-parentheses/description/
+    public List<String> generateParenthesis(int n) {
+        if (n == 0)
+            return Arrays.asList("");
+        if (n == 1)
+            return Arrays.asList("()");
+
+        StringBuilder sb = new StringBuilder();
+        List<String> res = new LinkedList<>();
+        backtracing22(res, sb, n, 0, n);
+        return res;
+    }
+
+    private void backtracing22(List<String> res, StringBuilder sb, int right, int left, int n) {
+        if (sb.length() == 2 * n) {
+            res.add(sb.toString());
+            return;
+        }
+        if (right > 0) {
+            sb.append("(");
+            right--;
+            left++;
+            backtracing22(res, sb, right, left, n);
+            sb.deleteCharAt(sb.length() - 1);
+            right++;
+            left--;
+        }
+        if (left > 0) {
+            sb.append(")");
+            left--;
+            backtracing22(res, sb, right, left, n);
+            sb.deleteCharAt(sb.length() - 1);
+            left++;
+        }
+    }
+
+    // 23 https://leetcode.com/problems/merge-k-sorted-lists/description/
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode o1, ListNode o2) {
+                return o1.val - o2.val;
+            }
+        });
+        for (ListNode l : lists) {
+            while (l != null) {
+                pq.add(l);
+                l = l.next;
+            }
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (!pq.isEmpty()) {
+            cur.next = pq.poll();
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+
+    private ListNode mergeKListsHelper(ListNode[] lists) {
+        int minV = Integer.MAX_VALUE;
+        int index = 0;
+        for (int i = 0; i < lists.length; i++) {
+            if (lists[i] != null && lists[i].val < minV) {
+                index = i;
+                minV = lists[i].val;
+            }
+        }
+        if (minV == Integer.MAX_VALUE) {
+            return null;
+        } else {
+            ListNode l = lists[index];
+            lists[index] = lists[index].next;
+            l.next = mergeKListsHelper(lists);
+            return l;
+        }
+    }
 }
