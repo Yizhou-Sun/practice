8c688888334217d064864a4bb7e566756599c599
Yizhou Sun <yizhous93@gmail.com>
2020-02-04 15:03:28 -0800

    58

--- src/com/pysolution/Solution_2.py
+++ src/com/pysolution/Solution_2.py
@@ -158,6 +158,32 @@ class Solution_2:
 
         return res
 
+    # 57 https://leetcode.com/problems/insert-interval/
+    def insert(self, intervals: List[List[int]],
+               newInterval: List[int]) -> List[List[int]]:
+        i = 0
+        while i < len(intervals):
+            if newInterval[0] <= intervals[i][0]:
+                break
+            i += 1
+
+        intervals.insert(i, newInterval)
+
+        res = [intervals[0]]
+        for i in range(1, len(intervals)):
+            if intervals[i][0] <= res[-1][1]:
+                res[-1][1] = max(res[-1][1], intervals[i][1])
+            else:
+                res.append(intervals[i])
+
+        return res
+
+    # 58 https://leetcode.com/problems/length-of-last-word/
+    def lengthOfLastWord(self, s: str) -> int:
+        s = s.strip()
+        words = s.split(" ")
+        return len(words[-1])
+
 
 if __name__ == "__main__":
     solution = Solution_2()
